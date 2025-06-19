e449dbb17d2cbd2c9d8a85a869f30ba13403ba4d
Rob Sewell <mrrobsewell@outlook.com>
2025-06-19 15:20:22 +0100

    so the tests are actually there

---
+++ before-demo.Tests.ps1
@@ -0,0 +1,44 @@
+Describe "These should all pass before the demo" {
+    BeforeAll {
+        $vars = Get-Variable
+        $workspacesTesting = Get-FabricWorkspace
+        $lakehousesTesting = $workspacesTesting | ForEach-Object {
+            Get-FabricLakehouse -WorkspaceId $_.id
+        }
+        $sqlDatabasesTesting = $workspacesTesting | ForEach-Object {
+            Get-FabricSqlDatabase -WorkspaceId $_.id
+        }
+        $dataPipelinesTesting = $workspacesTesting | ForEach-Object {
+            Get-FabricDataPipeline -WorkspaceId $_.id
+        }
+    }
+
+    Context "Session" {
+        It "Should not have the <_> variable" -TestCases @('securetoken', 'token', 'headers', 'body', 'apiParams', 'workspace', 'workspaces', 'capacity', 'params','workspaceName',
+        'workspaceId') {
+            $vars.Name | Should -Not -Contain $_
+        }
+    }
+    Context "Workspaces" {
+        It "Should not have the <_> workspace" -TestCases @('Workspace-API', 'dev', 'dev-dwh', 'test', 'test-dwh', 'prod', 'prod-dwh') {
+
+            $workspacesTesting.displayName | Should -Not -Contain $_
+        }
+    }
+    Context "Lakehouses" {
+        It "Should not have the <_> lakehouse" -TestCases @('LosAngelese', 'MyLakehouse') {
+
+            $lakehousesTesting.displayName | Should -Not -Contain $_
+        }
+    }
+    Context "SQL Databases" {
+        It "Should not have the <_> SQL Database" -TestCases @('MySqlDatabase') {
+            $SqlDatabasesTesting.displayName | Should -Not -Contain $_
+        }
+    }
+    Context "Data Pipelines" {
+        It "Should not have the <_> data pipeline" -TestCases @('MyPipeline','JefferySnoverKingOf') {
+            $dataPipelinesTesting.displayName | Should -Not -Contain $_
+        }
+    }
+}
